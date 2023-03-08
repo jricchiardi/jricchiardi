ccb76d7e15fb740e284300c0d4f7bc306bd34c00
Jose Luis Gadea <joseluisgadea@gmail.com>
2023-03-08 18:02:46 -0300

    comit cambio logos2

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -9,7 +9,7 @@ use yii\bootstrap\ActiveForm;
         <div class="container header-login-forgot">
 		<div class="row">
 			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                            <img src="<?= Yii::$app->urlManager->baseUrl ?>/images/logo-header.jpg" alt=""width="220px" height="70px" style='position:relative; top:120px;'  >
+                            <img src="<?= Yii::$app->urlManager->baseUrl ?>/images/logo-header.jpg" alt=""width="220px" height="50px" style='position:relative; top:120px;'  >
 			</div>
 		</div>
 		<div class="row">
